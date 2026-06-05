6b1e75828593460ef706d2505bcc31c21ab20981
eskila <eskil.andersen@gmail.com>
2026-06-05 13:03:19 -0400

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.py
@@ -0,0 +1,49 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # mutate in place to not add O(N) space
+        print(nums)
+        l = 0
+        n = len(nums) - 1
+        # We observe that adding a sorted left and right number, the
+        # left number is always smaller or equal to the right, so by adding a 3rd number in between,
+        # we can find the one that adds to create a zero-sum (or find that there's no solution for that triplet)
+        # (this number may be 0, which sums to zero when l == -r).
+        # How do we make sure we're not adding any duplicate triplets?
+        # A: When we find a result we move the pointers and skip over duplicate values,
+        # that way the valid combinations are always unique
+        result = []
+        while(l < n - 1):
+            num_l = nums[l]
+            # l + m + r = 0
+            # therefore l = -m - r
+            m = l + 1
+            r = n
+            while m < r:
+                num_m = nums[m]
+                num_r = nums[r]
+                if(num_r + num_m + num_l == 0): # Found a solution:
+                    result.append([num_l, num_m, num_r])
+                    # This pair of m, r should not reappear to avoid duplicates
+                    # so move the pointers
+                    # Will create a duplicate if both pointer point to the same numbers though
+                    m += 1
+                    r -= 1
+                    # Skip duplicates of m:
+                    while(m < r and nums[m] == nums[m-1]):
+                        m += 1
+                    # Skip duplicates of r:
+                    while(m < r and nums[r] == nums[r+1]):
+                        r -= 1
+                # walk m pointer right to get a larger number
+                elif(num_r + num_m + num_l < 0):
+                    m += 1
+                # Walk r pointer left to get a smaller number
+                else:
+                    r -= 1
+            # keep moving the pointer if there's a duplicate
+            while nums[l] == nums[l+1] and l < n - 1:
+                l += 1
+            # But always need to move the l pointer regardless
+            l += 1
+        return result
+        
